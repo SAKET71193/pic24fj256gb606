395dfdaf16483719151ce5895ec9344fe882fa9d
saket <saket71193@gmail.com>
2017-12-14 15:03:54 +0530

    oscillator.c updated two micros

--- oscillator.c
+++ oscillator.c
@@ -10,13 +10,18 @@ void Init_osc(void)
 { 
 
     OSCCONBITS OSCCONbitsCopy; // declare veriable to take backup 
+    
     OSCCONbits.COSC = 0b010; // Primary Oscillator (XT, HS, EC
     OSCCONbits.NOSC = 0b100; // Secondary Oscillator (SOSC)
     OSCCONbits.CLKLOCK = 1; //clock and pll section are lock
     OSCCONbits.IOLOCK = 0; //I/o lock is not active
-    OSCCONbits.CF = 1; //fscn has detect a clock failure 
-    OSCCONbits.POSCEN = 0; // primary oscillator disable during the  sleep
-      
+   // OSCCONbits.CF = 1; //fscn has detect a clock failure //Clock Fail Detect bit
+    OSCCONbits.POSCEN = 0; // primary oscillator disable during the  sleep  
+    OSCCONbits.SOSCEN = 1; //enable secondary oscillator 
 }
+
+
+
+
 
 
